541b1a9b323c51eae754af7a704a8c30f5d188d0
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2014-04-03 22:24:28 +0900

    firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/src/usb_config.h : Switch to USB full speed mode

--- firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/src/usb_config.h
+++ firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/src/usb_config.h
@@ -69,8 +69,8 @@
 //  is available on the target processor.
 //#define USB_TRANSCEIVER_OPTION USB_EXTERNAL_TRANSCEIVER
 
-//#define USB_SPEED_OPTION USB_FULL_SPEED
-#define USB_SPEED_OPTION USB_LOW_SPEED //(not valid option for PIC24F devices)
+#define USB_SPEED_OPTION USB_FULL_SPEED
+//#define USB_SPEED_OPTION USB_LOW_SPEED //(not valid option for PIC24F devices)
 
 #define MY_VID 0x04D8
 #define MY_PID 0xF550   // Esrille New Keyboard
