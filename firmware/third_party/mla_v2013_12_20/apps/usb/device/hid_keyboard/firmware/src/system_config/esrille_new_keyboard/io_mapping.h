ecb675403722c9bc5bab807dbd653c6a4f15c4fa
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2014-04-12 23:08:18 +0900

    firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/src/system_config/esrille_new_keyboard/io_mapping.h (self_power) : Remove

--- firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/src/system_config/esrille_new_keyboard/io_mapping.h
+++ firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/src/system_config/esrille_new_keyboard/io_mapping.h
@@ -28,6 +28,3 @@
 
 #define BUTTON_USB_DEVICE_HID_KEYBOARD_KEY              BUTTON_S2
 #define BUTTON_USB_DEVICE_REMOTE_WAKEUP                 BUTTON_S2
-
-/* USB Stack I/O options. */
-#define self_power                                      1
