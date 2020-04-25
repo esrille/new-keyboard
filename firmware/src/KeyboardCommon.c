8cdfdf6eedaf78d742cdbf95b8e1274f007811ad
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2020-04-25 23:51:09 +0900

    Reorganize configurations for NISSE Rev. 6
    The followings are the new configuration names for NISSE Rev. 6:
    
    PIC18F47J53_NISSE_BLE
    PIC18F47J53_NISSE_BLE_TSAP
    PIC18F47J53_NISSE_USB
    PIC18F47J53_NISSE_USB_TSAP
    
    firmware/src/KeyboardCommon.c
    firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/MPLAB.X/nbproject/configurations.xml
    firmware/third_party/mla_v2013_12_20/apps/usb/device/hid_keyboard/firmware/src/system_config/pic18f47j53_nisse/fixed_address_memory.h : Set the firmware version number to 1.05

--- firmware/src/KeyboardCommon.c
+++ firmware/src/KeyboardCommon.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2017 Esrille Inc.
+ * Copyright 2013-2020 Esrille Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -382,7 +382,7 @@ static const uint8_t about_ver[] = {
     KEY_V, KEY_E, KEY_R, KEY_PERIOD, KEY_SPACEBAR, 0
 };
 static const uint8_t about_copyright[] = {
-    KEY_C, KEY_O, KEY_P, KEY_Y, KEY_R, KEY_I, KEY_G, KEY_H, KEY_T, KEY_SPACEBAR, KEY_2, KEY_0, KEY_1, KEY_3, KEY_MINUS, KEY_2, KEY_0, KEY_1, KEY_7, KEY_SPACEBAR,
+    KEY_C, KEY_O, KEY_P, KEY_Y, KEY_R, KEY_I, KEY_G, KEY_H, KEY_T, KEY_SPACEBAR, KEY_2, KEY_0, KEY_1, KEY_3, KEY_MINUS, KEY_2, KEY_0, KEY_2, KEY_0, KEY_SPACEBAR,
     KEY_E, KEY_S, KEY_R, KEY_I, KEY_L, KEY_L, KEY_E, KEY_SPACEBAR, KEY_I, KEY_N, KEY_C, KEY_PERIOD, KEY_ENTER, 0
 };
 static const uint8_t about_f2[] = {
